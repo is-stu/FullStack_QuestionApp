c184c7c531745c813af20076b5d6808e03baa85c
Stewar <stewar1020@gmail.com>
2021-05-25 12:25:20 -0500

    adding create useCase

---
+++ src/main/java/co/com/sofka/questions/routers/QuestionRoutes.java
@@ -0,0 +1,31 @@
+package co.com.sofka.questions.routers;
+
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.usecases.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class QuestionRoutes {
+
+    @Bean
+    public RouterFunction<ServerResponse> create(CreateUseCase createUseCase) {
+        return route(POST("/create").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(QuestionDTO.class)
+                        .flatMap(questionDTO -> createUseCase.apply(questionDTO)
+                                .flatMap(result -> ServerResponse.ok()
+                                        .contentType(MediaType.TEXT_PLAIN)
+                                        .bodyValue(result))
+                        )
+        );
+    }
+
+}
