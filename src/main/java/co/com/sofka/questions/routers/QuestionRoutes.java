0ee3128b8bdd2cf0468e3b3ab51c24a6410e6f6d
Stewar <stewar1020@gmail.com>
2021-05-25 15:33:07 -0500

    Adding questions usesCases

--- src/main/java/co/com/sofka/questions/routers/QuestionRoutes.java
+++ src/main/java/co/com/sofka/questions/routers/QuestionRoutes.java
@@ -1,6 +1,5 @@
 package co.com.sofka.questions.routers;
 
-import co.com.sofka.questions.model.AnswerDTO;
 import co.com.sofka.questions.model.QuestionDTO;
 import co.com.sofka.questions.usecases.*;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +16,7 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class QuestionRoutes {
 
     @Bean
-    public RouterFunction<ServerResponse> create(CreateUseCase createUseCase) {
+    public RouterFunction<ServerResponse> createQuestion(CreateUseCase createUseCase) {
         return route(POST("/create").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(QuestionDTO.class)
                         .flatMap(questionDTO -> createUseCase.apply(questionDTO)
@@ -25,6 +24,39 @@ public class QuestionRoutes {
                                         .contentType(MediaType.TEXT_PLAIN)
                                         .bodyValue(result))
                         )
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getQuestion(GetQuestionUseCase getUseCase) {
+        return route(
+                GET("/get/{id}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getUseCase.apply(
+                                request.pathVariable("id")),
+                                QuestionDTO.class
+                        ))
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> delete(DeleteUseCase deleteUseCase) {
+        return route(
+                DELETE("/delete/{id}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.accepted()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(deleteUseCase.apply(request.pathVariable("id")), Void.class))
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getAllQuestion(GetAllQuestions listUseCase) {
+        return route(
+                GET("/getAll").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(listUseCase.get(), QuestionDTO.class))
         );
     }
 
